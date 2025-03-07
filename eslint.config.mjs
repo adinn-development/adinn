46235ec15371a148a4bac5ac8387276d33684e6e
BalaAyyappan1 <balaayyappan11@gmail.com>
2025-03-07 22:30:22 +0530

    eslint error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,19 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc'
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
 
-export default [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next'],
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      "@next/next/no-img-element": "off"
     },
-  },
-];
+  }),
+]
+
+export default eslintConfig
