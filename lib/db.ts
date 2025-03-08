889acc938f3274aa5def14f67bf8d88d9bd13ae4
BalaAyyappan1 <balaayyappan11@gmail.com>
2025-03-08 16:12:04 +0530

    hero pending

--- lib/db.ts
+++ lib/db.ts
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 
-console.log(MONGODB_URI);
-
 // if (!MONGODB_URI) {
 //   throw new Error("Please define the MONGODB_URI environment variable inside .env");
 // }
@@ -11,7 +9,7 @@ console.log(MONGODB_URI);
 let cached = global.mongoose;
 
 if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+  cached = global.mongoose = { connection: null, conn: null, promise: null };
 }
 
 async function connectDB() {
