490e94dcfc3ba40f3939063088ebb7c05a9848be
BalaAyyappan1 <balaayyappan11@gmail.com>
2025-03-10 14:35:40 +0530

    service name

--- components/AboutUs/Hero.tsx
+++ components/AboutUs/Hero.tsx
@@ -7,8 +7,10 @@ const Hero = () => {
       className="relative w-full h-screen bg-cover bg-center bg-no-repeat"
       style={{ backgroundImage: `url(${AboutHero.src})` }}
     >
-      <TopNav />
-
+     <div className="relative z-10">
+        <TopNav />
+      </div>
+{/* 
       <div className="absolute bottom-0 left-0 p-6 sm:p-8 text-white max-w-lg">
         <h1 className="text-[54px] font-bold leading-tight drop-shadow-[0_0_10px_rgba(207,30,0,0.5)]">
           About Us
@@ -16,7 +18,33 @@ const Hero = () => {
         <p className="text-[#E1E1E1] text-[20px] leading-tight">
           Lorem ipsum dolor sit amet consectetur.
         </p>
+      </div> */}
+            <div className="absolute inset-0 flex items-center justify-center -mt-10">
+        <div className="flex flex-col items-center text-center">
+          <div className="text-white">
+            <h1 className="text-[54px] font-bold leading-tight instrument-font">
+              About Us
+            </h1>
+            <p className="text-[54px] text-white">
+            Lorem ipsum dolor
+            </p>
+          </div>
+{/* 
+          <div className="mt-6 flex space-x-4">
+            <Link href="/projects">
+              <button className="bg-[#000000] cursor-pointer text-white px-6 py-3 rounded-full w-[180px] h-[48px] text-center transition-all hover:bg-[#333333] hover:shadow-lg">
+                View Projects
+              </button>
+            </Link>
+            <Link href="/contact">
+              <button className="bg-transparent cursor-pointer border border-[#EEEEEE] text-[#EEEEEE] px-6 py-3 rounded-full w-[180px] h-[48px] text-center transition-all hover:bg-[#EEEEEE]/10 hover:border-white hover:text-white hover:shadow-lg">
+                Contact us
+              </button>
+            </Link>
+          </div> */}
+        </div>
       </div>
+
     </div>
   );
 };
