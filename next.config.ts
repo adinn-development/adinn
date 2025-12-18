21c1280f6dcf28d3e4f9bbe8fc9990641a74437f
Sathishkumar Dhanasekaran <srbedev@adinn.co.in>
2025-12-18 16:37:56 +0530

    next config file update

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,25 @@
 
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          { type: 'host', value: 'adinn-three.vercel.app' },
+        ],
+        destination: 'https://www.adinn.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
