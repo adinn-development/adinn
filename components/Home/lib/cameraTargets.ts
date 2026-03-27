1c7fdef2afa8493788658fd3014e68171fbc3be4
Sathishkumar Dhanasekaran <srbedev@adinn.co.in>
2026-03-27 16:16:54 +0530

    format document

--- components/Home/lib/cameraTargets.ts
+++ components/Home/lib/cameraTargets.ts
@@ -7,15 +7,11 @@ export type CameraTargetConfig = {
 
 export const cameraTargets: Record<string, CameraTargetConfig> = {
   roadshow: {
-    position: new THREE.Vector3(
-      -16.651605760184015,
-      6.8,
-      16.2,
-    ),
+    position: new THREE.Vector3(-16.651605760184015, 6.8, 16.2),
     target: new THREE.Vector3(-2.3, 1.0, -3),
   },
 
- wallPainting: {
+  wallPainting: {
     position: new THREE.Vector3(
       -11.841159988826876,
       2.96936501008872,
@@ -28,7 +24,7 @@ export const cameraTargets: Record<string, CameraTargetConfig> = {
     ),
   },
 
-    digitalMarketing: {
+  digitalMarketing: {
     position: new THREE.Vector3(
       -7.3771064604067895,
       4.538773150841211,
@@ -42,31 +38,28 @@ export const cameraTargets: Record<string, CameraTargetConfig> = {
   },
 
   fixtures: {
-  position: new THREE.Vector3(
-    -7.429264986539309,
-    4.217257581676869,
-    9.44234572930943,
-  ),
-  target: new THREE.Vector3(
-    1.6995825782370575,
-    3.0244508485889363e-19,
-    -2.5660661861958483,
-  ),
-},
+    position: new THREE.Vector3(
+      -7.429264986539309,
+      4.217257581676869,
+      9.44234572930943,
+    ),
+    target: new THREE.Vector3(
+      1.6995825782370575,
+      3.0244508485889363e-19,
+      -2.5660661861958483,
+    ),
+  },
 
-event: {
-  position: new THREE.Vector3(
-6.964541968847004,
-1.3622833748347147,
-14.450129034010697
-
-  ),
-  target: new THREE.Vector3(
-  3.8341241764816263,
--5.795819418764129e-19,
--1.3231030714503371
-  ),
-},
-
-
+  event: {
+    position: new THREE.Vector3(
+      6.964541968847004,
+      1.3622833748347147,
+      14.450129034010697,
+    ),
+    target: new THREE.Vector3(
+      3.8341241764816263,
+      -5.795819418764129e-19,
+      -1.3231030714503371,
+    ),
+  },
 };
