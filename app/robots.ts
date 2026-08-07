13c8717a5e016b851bbba13e0c21b8fb7d4e5d71
thendral <webdeveloper1@adinn.co.in>
2026-08-07 11:57:34 +0530

    Add AI crawler allowlist to robots.ts and remove delete-account URL from sitemap

---
+++ app/robots.ts
@@ -0,0 +1,46 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/cgi-bin/',
+      },
+      {
+        userAgent: 'GPTBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'ChatGPT-User',
+        allow: '/',
+      },
+      {
+        userAgent: 'ClaudeBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'Claude-Web',
+        allow: '/',
+      },
+      {
+        userAgent: 'PerplexityBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'Google-Extended',
+        allow: '/',
+      },
+      {
+        userAgent: 'Applebot-Extended',
+        allow: '/',
+      },
+      {
+        userAgent: 'CCBot',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://www.adinn.com/sitemap.xml',
+  }
+}
